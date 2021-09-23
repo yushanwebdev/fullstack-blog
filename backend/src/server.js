4d35d0527721fa3dde3cb22a591b4d1e66b08b77
yushan <yushan@intervest.lk>
2021-09-23 20:50:28 +0530

    feat: Develop the Server to save comments in the fake database

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,15 @@ import express from "express";
 const articlesInfo = {
   "learn-react": {
     upvotes: 0,
+    comments: [],
   },
   "learn-node": {
     upvotes: 0,
+    comments: [],
   },
   "my-thoughts-on-resumes": {
     upvotes: 0,
+    comments: [],
   },
 };
 
@@ -24,6 +27,18 @@ app.post("/api/articles/:name/upvote", (req, res) => {
   res
     .status(200)
     .send(`${articleName} now has ${articlesInfo[articleName].upvotes} votes.`);
+});
+
+app.post("/api/articles/:name/add-comment", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].comments.push({
+    username,
+    text,
+  });
+
+  res.status(200).send(articlesInfo[articleName]);
 });
 
 app.listen(5000, () => console.log("Listening on port 5000"));
