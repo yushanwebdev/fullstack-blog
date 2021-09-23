87787bd95afcdfdf85cae9bbcda557997377546b
yushan <yushan@intervest.lk>
2021-09-23 19:26:28 +0530

    feat: Develop the functionality to upvote the articles in Server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,29 @@
 import express from "express";
 
+const articlesInfo = {
+  "learn-react": {
+    upvotes: 0,
+  },
+  "learn-node": {
+    upvotes: 0,
+  },
+  "my-thoughts-on-resumes": {
+    upvotes: 0,
+  },
+};
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/hello", (req, res) => res.send("Hello!"));
+app.post("/api/articles/:name/upvote", (req, res) => {
+  const articleName = req.params.name;
 
-app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
+  articlesInfo[articleName].upvotes += 1;
 
-app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}!`));
+  res
+    .status(200)
+    .send(`${articleName} now has ${articlesInfo[articleName].upvotes} votes.`);
+});
 
 app.listen(5000, () => console.log("Listening on port 5000"));
