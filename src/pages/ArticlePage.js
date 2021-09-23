b1509c85aa87e9a567da823464be7f1d51477feb
yushan <yushan@intervest.lk>
2021-09-23 12:43:26 +0530

    feat: Develop the functionality to update the content of <ArticlePage /> according to the URL

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,3 +1,19 @@
-export default function ArticlePage() {
-  return <h1>This is an article</h1>;
+import ArticleContent from "../article-content";
+
+export default function ArticlePage({ match }) {
+  const name = match.params.name;
+  const article = ArticleContent.find((item) => item.name === name);
+
+  if (!article) {
+    return <h1>Article does not exist!</h1>;
+  }
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((item, key) => (
+        <p key={key}>{item}</p>
+      ))}
+    </>
+  );
 }
