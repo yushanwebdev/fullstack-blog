909726b0dd2c6235c60ec15130bcbc37685a547b
yushan <yushan@intervest.lk>
2021-09-23 12:09:37 +0530

    feat: Create the <AboutPage />

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,37 @@
+export default function AboutPage() {
+  return (
+    <>
+      <h1>About Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum
+        metus eros, vitae eleifend risus varius a. Sed sollicitudin, nulla vel
+        finibus finibus, quam lectus hendrerit nulla, ut ornare augue nisi vel
+        dolor. Sed molestie tempor augue non pretium. Duis cursus sit amet ante
+        nec sodales. Donec maximus porta sem, nec tempor mi euismod in. Nullam
+        tincidunt quam et euismod condimentum. Phasellus et purus et urna
+        aliquet mollis. Sed sed elit sed sem euismod vulputate. Sed vel lacus
+        hendrerit, commodo neque ut, facilisis felis.
+      </p>
+      <p>
+        Cras at dictum neque, eu maximus sem. Quisque et hendrerit neque. Fusce
+        eget lacus hendrerit, rhoncus ipsum aliquam, gravida odio. Phasellus
+        varius lorem at condimentum vehicula. Fusce at finibus arcu. Proin
+        accumsan sagittis lectus sit amet sollicitudin. Vivamus consectetur
+        auctor leo, vel vehicula arcu bibendum vel. Aliquam semper tristique
+        urna id tincidunt. Aliquam rhoncus eros mauris, vitae ullamcorper ipsum
+        tristique at. Nulla facilisi. Suspendisse pretium efficitur tellus in
+        fringilla.
+      </p>
+      <p>
+        Donec vel blandit sem, ut viverra felis. Nam a mi aliquet, viverra mi
+        nec, volutpat leo. Proin euismod viverra massa non scelerisque. Praesent
+        eget leo nisi. Ut porta leo vel dui hendrerit vestibulum vel ac ante.
+        Sed et ante sed lorem malesuada finibus semper quis diam. Duis ac risus
+        augue. Nulla nec faucibus libero. Fusce id dignissim quam. Fusce dictum
+        ac ante ac porta. Nam sit amet venenatis tellus, vitae elementum neque.
+        Vivamus malesuada maximus metus, in consequat magna vulputate eu.
+        Pellentesque rhoncus sollicitudin elit a tempor.
+      </p>
+    </>
+  );
+}
