947d01c12cb5bf17667b5a26c4a268b8fc0fe6dd
yushan <yushan@intervest.lk>
2021-09-23 18:48:34 +0530

    feat: Develop the Server to handle `/hello/:name` GET request

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,8 @@ app.use(express.json());
 
 app.get("/hello", (req, res) => res.send("Hello!"));
 
+app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
+
 app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}!`));
 
 app.listen(5000, () => console.log("Listening on port 5000"));
