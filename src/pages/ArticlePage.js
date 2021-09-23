0648e770dc709f1888783b66bac51c24a7ebb290
yushan <yushan@intervest.lk>
2021-09-23 14:22:20 +0530

    feat: Develop Other Articles section in the <ArticlePage />

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,8 +1,10 @@
 import articleContent from "../article-content";
+import ArticleList from "../components/ArticleList";
 
 export default function ArticlePage({ match }) {
   const name = match.params.name;
   const article = articleContent.find((item) => item.name === name);
+  const otherArticles = articleContent.filter((item) => item.name !== name);
 
   if (!article) {
     return <h1>Article does not exist!</h1>;
@@ -14,6 +16,8 @@ export default function ArticlePage({ match }) {
       {article.content.map((item, key) => (
         <p key={key}>{item}</p>
       ))}
+      <h3>Other Articles</h3>
+      <ArticleList articles={otherArticles} />
     </>
   );
 }
